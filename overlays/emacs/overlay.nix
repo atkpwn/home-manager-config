ddca01107edc8933b467dcdcb9d35ec18527beda
Attakorn Putwattana <atkpwn@gmail.com>
2024-07-30 00:47:17 +0200

    use emacs-macport on darwin

--- overlays/emacs/overlay.nix
+++ overlays/emacs/overlay.nix
@@ -1,17 +1,22 @@
 self: pkgs:
 
 let
+  myEmacs = if pkgs.stdenv.hostPlatform.isLinux then
+    pkgs.emacs-gtk
+  else
+    pkgs.emacs-macport;
+
   # xwidgets requires GTK as X window toolkit
-  myEmacs = pkgs.emacs-gtk.overrideAttrs (o: {
+  myEmacsWithXWidgets = myEmacs.overrideAttrs (o: {
     # see example configure flags from jwiegley
     # https://github.com/jwiegley/nix-config/blob/d48861de5d2d52e84403213a6110389c69c0bb4c/overlays/10-emacs.nix#L700
     configureFlags = o.configureFlags ++ [
       "--with-xwidgets"
     ];
   });
-  myEmacsPackages   = import ./packages.nix pkgs;
-  emacsWithPackages = (pkgs.emacsPackagesFor myEmacs).emacsWithPackages;
-in
-{
+  myEmacsPackages = import ./packages.nix pkgs;
+  emacsWithPackages =
+    (pkgs.emacsPackagesFor myEmacsWithXWidgets).emacsWithPackages;
+in {
   myEmacsWithPackages = (emacsWithPackages myEmacsPackages);
 }
