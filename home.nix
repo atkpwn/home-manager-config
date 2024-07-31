688f10c3d4d3e8d6e656badc692b119ed8da610f
Attakorn Putwattana <atkpwn@gmail.com>
2024-08-01 00:18:12 +0200

    basic xfce setup

--- home.nix
+++ home.nix
@@ -1,7 +1,7 @@
 { config, pkgs, lib, nix-colors, ... }:
 
 let
-  linuxAttrs = lib.optionalAttrs pkgs.stdenv.targetPlatform.isLinux;
+  linuxAttrs = lib.optionalAttrs pkgs.stdenv.hostPlatform.isLinux;
 in {
   imports = [
     nix-colors.homeManagerModules.default
@@ -43,18 +43,30 @@ in {
 
   fonts.fontconfig.enable = true;
 
-  xdg.enable = true;
+  xdg = {
+    enable = true;
+    # stop beeping
+    configFile."autostart/beep.desktop".text = builtins.readFile ./config/beep.desktop;
+  };
 
   # https://tinted-theming.github.io/base16-gallery/
   colorScheme = nix-colors.colorSchemes.ia-dark;
 
   xfconf.settings = linuxAttrs {
+    xfwm4 = {
+      "general/workspace_count" = 2;
+    };
     xfce4-keyboard-shortcuts = {
       "xfwm4/custom/<Super>Down"   = "tile_down_key";
       "xfwm4/custom/<Super>Up"     = "tile_up_key";
       "xfwm4/custom/<Super>Left"   = "tile_left_key";
       "xfwm4/custom/<Super>Right"  = "tile_right_key";
       "xfwm4/custom/<Super>Return" = "maximize_window_key";
+      "xfwm4/custom/<Primary><Alt><Shift>Left"  = "move_window_prev_workspace_key";
+      "xfwm4/custom/<Primary><Alt><Shift>Right" = "move_window_next_workspace_key";
+    };
+    xsettings = {
+      "Net/ThemeName" = "Zukitre-dark";
     };
   };
 }
--- packages.nix
+++ packages.nix
@@ -13,6 +13,7 @@ let
     xclip
     zoxide
     zstd
+    zuki-themes
   ];
 
   emacsWithTools = [
