205e24e5333b1585013243132ae057312e1e8ba9
Attakorn Putwattana <atkpwn@gmail.com>
2024-12-26 18:18:23 +0100

    add xfce and rofi modules

---
+++ modules/rofi.nix
@@ -0,0 +1,32 @@
+{ pkgs, ... }:
+let
+  defaultFont = "JetBrainsMono Nerd Font";
+in
+{
+  programs.rofi = {
+    enable = true;
+    cycle = true;
+    extraConfig = {
+      modi = "run,filebrowser,emoji";
+      font = defaultFont + " 14";
+      kb-cancel = "!Alt+space,Escape,Control+g";
+      display-combi = " ";
+      display-emoji = "󰱨 ";
+      display-filebrowser = " ";
+      display-run = " ";
+      display-window = " ";
+      display-workspace = "";
+      # drun-display-format = "{name}";
+      show-icons = true;
+
+      sidebar-mode = true;
+      window-format = "{c} · {t}";
+    };
+    location = "center";
+    plugins = [
+      pkgs.rofi-emoji
+    ];
+    terminal = "alacritty";
+    theme = ../config/rofi/tomorrow-night.rasi;
+  };
+}
--- programs.nix
+++ programs.nix
@@ -298,33 +298,6 @@ in {
     };
   };
 
-  rofi = {
-    enable = isLinux;
-    cycle = true;
-    extraConfig = {
-      modi = "run,filebrowser,emoji";
-      font = defaultFont + " 14";
-      kb-cancel = "!Alt+space,Escape,Control+g";
-      display-combi = " ";
-      display-emoji = "󰱨 ";
-      display-filebrowser = " ";
-      display-run = " ";
-      display-window = " ";
-      display-workspace = "";
-      # drun-display-format = "{name}";
-      show-icons = true;
-
-      sidebar-mode = true;
-      window-format = "{c} · {t}";
-    };
-    location = "center";
-    plugins = [
-      pkgs.rofi-emoji
-    ];
-    terminal = "alacritty";
-    theme = ./config/rofi/tomorrow-night.rasi;
-  };
-
   starship = {
     enable = true;
     enableBashIntegration = true;
